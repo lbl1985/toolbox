aa9278c5588b9c282ddf12ac4dcb0c2699dea14c
Binlong Li <herbert19lee@gmail.com>
2011-09-07 07:03:04 -0400

    remove getProjectBaseFolder. It should be project orientated.

--- matlab/getProjectBaseFolder.m
+++
@@ -1,4 +0,0 @@
-function projectBaseFolder = getProjectBaseFolder
-projectBaseFolder = which('denseFlag.m');
-projectBaseFolder = projectBaseFolder(1:strfind(projectBaseFolder, 'denseFlag.m') - 1);
- 
