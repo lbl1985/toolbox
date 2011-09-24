a6d50e9c318e21f123cf1eecdfba3cc65ac5560a
Binlong Li <herbert19lee@gmail.com>
2011-09-24 15:43:09 -0400

    separate the divideH_HtFrob and divideH_Ht

--- numeric/dividedH_HtFrob.m
+++ numeric/dividedH_HtFrob.m
@@ -1,6 +1,6 @@
-function normFrob = dividedH_HtFrob(A, hankelWindowSize)
+function normFrob = dividedH_HtFrob(symMatrix, hankelWindowSize)
 % m = hankelWindowSize;
 
-symMatrix = dividedH_Ht(A, hankelWindowSize);
+% symMatrix = dividedH_Ht(A, hankelWindowSize);
 w = [ones(1, 2 * hankelWindowSize) 2 * ones(1, length(2*hankelWindowSize+1 : size(symMatrix, 1)))];
 normFrob = sqrt(w * (symMatrix.^2));
--- numeric/test/dividedH_HtFrob_test.m
+++ numeric/test/dividedH_HtFrob_test.m
@@ -9,6 +9,7 @@ AHankel = [hankel(A(1:m), A(m : n/2)); hankel(A(n/2+1 : n/2+m), A(n/2+m : n))];
 AHankelSquare = AHankel * AHankel';
 AHankelSquareFrob = norm(AHankelSquare, 'fro');
 
-normFrob = dividedH_HtFrob(A, m);
+symMatrix = dividedH_Ht(A, m);
+normFrob = dividedH_HtFrob(symMatrix, m);
 c = comparedata(AHankelSquareFrob, normFrob);
 assert(isequal(c, 1), 'dividedH_HtFrob is broken.');
