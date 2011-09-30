b8ebb44d8bc3311a980d6fcced91b48ea57c1780
Binlong Li <herbert19lee@gmail.com>
2011-09-30 14:41:52 -0400

    add randomInstance. Details usage in the comments.

---
+++ matlab/randomInstances.m
@@ -0,0 +1,28 @@
+function Xtrain_raw_sub = randomInstances(trainFeature, criterionNum)
+% Input:
+% trainFeature: Input Feature array with column wises features.
+% criterionNum: artifical number of criterion you may need to sampling.
+% Output:
+% Xtrain_raw_sub: Random sampled instances matrix
+% Related: kmeansPackage.m
+% Author: Binlong Li    30 Sep 2011
+
+tot_num_samples = size(trainFeature, 2);
+
+fprintf('total number of training samples: %d\n', tot_num_samples);
+
+criterionNum = [criterionNum tot_num_samples];
+
+sub_size = min(criterionNum);
+
+fprintf('kmeans on number of samples: %d\n', sub_size);
+
+rand('state', params.seed);
+
+ridx = randperm(tot_num_samples);
+ridx = ridx(1:sub_size);
+
+line = false(tot_num_samples, 1);
+line(ridx) = true;
+
+Xtrain_raw_sub = trainFeature(:, line);
