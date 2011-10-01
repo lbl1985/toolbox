973d7b568931dd0345fdcc70c61e42899b7c8005
Binlong Li <herbert19lee@gmail.com>
2011-10-01 10:59:01 -0400

    fix seed input problem for randomInstances.

--- matlab/randomInstances.m
+++ matlab/randomInstances.m
@@ -1,4 +1,4 @@
-function Xtrain_raw_sub = randomInstances(trainFeature, criterionNum)
+function Xtrain_raw_sub = randomInstances(trainFeature, criterionNum, seed)
 % Input:
 % trainFeature: Input Feature array with column wises features.
 % criterionNum: artifical number of criterion you may need to sampling.
@@ -17,7 +17,7 @@ sub_size = min(criterionNum);
 
 fprintf('kmeans on number of samples: %d\n', sub_size);
 
-rand('state', params.seed);
+rand('state', seed);
 
 ridx = randperm(tot_num_samples);
 ridx = ridx(1:sub_size);
