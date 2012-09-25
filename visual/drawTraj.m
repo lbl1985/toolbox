1af06781c0be083f4093c2d7ab70a1b175b8280c
Binlong Li <herbert19lee@gmail.com>
2012-09-25 13:33:48 -0400

    script for drawing trajectories.

---
+++ visual/drawTraj.m
@@ -0,0 +1,17 @@
+function drawTraj(traj)
+% traj: n x 2 matrix, n = 1, stands for beginning, n = end, stands for
+% ending.
+% The Drawed trajectory will be with blue line and red star at the end
+% point.
+%
+% Trial Code, also illustrate the relationship between coordinates
+% a = [ 10 20; 20 40; 40 80; 80 160; 160 320];
+% imshow('~/Desktop/lena.jpg')
+% figure(gcf); 
+% hold on; 
+% line(a(:, 1), a(:, 2)); plot(a(end, 1), a(end, 2), 'r*');
+% hold off; 
+% figure(fig); hold on; 
+line(traj(:, 1), traj(:, 2)); plot(traj(end, 1), traj(end, 2), 'r*');
+% line(traj(:, 2), traj(:, 1)); plot(traj(end, 2), traj(end, 1), 'r*');
+% hold off;
