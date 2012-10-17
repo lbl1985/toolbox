afc9e73c60426114dd5b2fd73e70999ebbbc7939
Binlong Li <herbert19lee@gmail.com>
2012-10-16 21:49:59 -0400

    adding help information and be able to handle xmlfile with abosule path.

--- cvml/readcvml.m
+++ cvml/readcvml.m
@@ -1,5 +1,21 @@
 function readcvml(xmlfile, savingFolder)
-filename = xmlfile(1:end-4);
+% Read .xml file, which is written in CVML format, then convert and saved
+% as .mat file.
+% Input: 
+% xmlfile:      .xml file name. It could include the absolute path or only the
+%               file name lies in the current folder
+% savingFolder: where the .mat file will be saved. 
+% Output: 
+% .mat file     live in savingFolder. 
+% 
+% Author:       Binlong Li      16 Oct 2012     21:45
+ind = strfind(xmlfile, '/');
+if ~isempty(ind)
+    filename = xmlfile(ind(end)+1:end-4);
+else
+    filename = xmlfile(1:end-4);
+end
+
 tgtFile = [filename '.csv'];  
 comm = ['!xsltproc /Users/herbert19lee/Documents/MATLAB/toolbox/cvml/cvml_csv.xslt ' xmlfile '> ' fullfile(savingFolder, tgtFile)];
 eval(comm);
